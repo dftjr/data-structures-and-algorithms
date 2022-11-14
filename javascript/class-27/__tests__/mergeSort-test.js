2196f036ea8bfbfec4f5df12d663444bc46775bf
DAVID TUSIA <DAVE.TUSIA@GMAIL.COM>
2022-11-14 10:32:28 +0000

    Code completed. All tests passing.

--- javascript/class-27/__tests__/mergeSort-test.js
+++ javascript/class-27/__tests__/mergeSort-test.js
@@ -1,15 +1,15 @@
 'use strict';
 
-const insertionSort = require('../insertionSort')
+const mergeSort = require('../mergeSort');
 
 const evenArr = [8,4,23,42,16,15];
 const oddArr = [5,4,3,2,1];
 
 describe('Testing the insertionSort function', () => {
   test('Should return a sorted even array from the one that was entered', () => {
-    expect(insertionSort(evenArr)).toEqual([4,8,15,16,23,42]);
+    expect(mergeSort(evenArr)).toEqual([4,8,15,16,23,42]);
   });
   test('Should return a sorted odd array from the one that was entered', () => {
-    expect(insertionSort(oddArr)).toEqual([1,2,3,4,5]);
+    expect(mergeSort(oddArr)).toEqual([1,2,3,4,5]);
   });
 });
