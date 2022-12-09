78604bf9f2f70a43228a07a8391194a74ddafaa4
DAVID TUSIA <DAVE.TUSIA@GMAIL.COM>
2022-12-09 05:04:12 +0000

    Initial push

--- javascript/reverse-array/__tests__/reverseArray-test.js
+++ javascript/reverse-array/__tests__/reverseArray-test.js
@@ -11,7 +11,7 @@ describe('Testing the reverse-array function', () => {
     expect(reverseArray(num1)).toEqual([6, 5, 4, 3, 2, 1]);
   });
   test('Can successfully reverse a int array with negatives', () => {
-    expect(reverseArray(num2)).toEqual([-12, 823, -923, 10,23,3546,2354,89,]);
+    expect(reverseArray(num2)).toEqual([-12, 823, -923, 10, 23, 3546, 2354, 89]);
   });
   test('Can successfully reverse a str array', () => {
     expect(reverseArray(str1)).toEqual(["F", "E", "D", "C", "B", "A"]);
